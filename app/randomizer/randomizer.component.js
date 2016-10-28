888e3c036651c812256c861aadbd9b88129b3ad3
kelvinatorr <kelvin.dealca@gmail.com>
2016-10-28 13:04:15 -0700

    show spinner when waiting for server data

--- app/randomizer/randomizer.component.js
+++ app/randomizer/randomizer.component.js
@@ -15,18 +15,24 @@
             date: new Date()
         };
 
-        self.scheduleData = Schedule.data;
-
         self.selectedSchedule = null;
 
+        self.gettingData = false;
+
         self.randomize = function(formModel) {
+            if(self.gettingData) return;
+
+            self.gettingData = true;
+
             Schedule.getData(formModel).then(function() {
                 self.scheduleData = Schedule.data;
                 var selectedIndex = Math.floor(Math.random() * self.scheduleData.length);
+                self.gettingData = false;
                 // todo randomly different schedules for 3 seconds
-                //
+                // show randomly selected schedule
                 self.selectedSchedule = self.scheduleData[selectedIndex];
             });
+
         }
 
     }
