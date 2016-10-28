75ca315b0a0246b71da3cfb571f65ec6fec397f5
kelvinatorr <kelvin.dealca@gmail.com>
2016-10-28 12:54:09 -0700

    display randomly selected schedule

--- app/randomizer/randomizer.component.js
+++ app/randomizer/randomizer.component.js
@@ -1,4 +1,4 @@
-(function() {
+(function(Math) {
     'use strict';
 
     angular.module('tcrApp')
@@ -22,8 +22,12 @@
         self.randomize = function(formModel) {
             Schedule.getData(formModel).then(function() {
                 self.scheduleData = Schedule.data;
+                var selectedIndex = Math.floor(Math.random() * self.scheduleData.length);
+                // todo randomly different schedules for 3 seconds
+                //
+                self.selectedSchedule = self.scheduleData[selectedIndex];
             });
         }
 
     }
-})();
+})(Math);
