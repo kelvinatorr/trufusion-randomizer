602b96fee206b303c194d7e390ea14a1b9174054
kelvinatorr <kelvin.dealca@gmail.com>
2016-10-31 15:33:21 -0700

    Add list to display the schedule data

--- app/schedule-display/schedule-display.component.js
+++ app/schedule-display/schedule-display.component.js
@@ -7,11 +7,43 @@
             controller: ScheduleDisplay
         });
 
-    function ScheduleDisplay(Schedule) {
-        this.submit = function(formModel) {
-            console.log('submitted');
-        };
+    function ScheduleDisplay(Schedule, $mdDialog) {
+         var $ctrl = this;
+
+        this.submit = submit;
 
         this.scheduleData = Schedule.data;
+
+        this.filter = {
+            location: 'Eastern',
+            date: new Date()
+        };
+
+        this.gettingData = false;
+
+        function submit(formModel) {
+            if($ctrl.gettingData) return;
+            // hide the list
+            $ctrl.gettingData = true;
+            // make the request
+            Schedule.getData(formModel).then(function() {
+                $ctrl.scheduleData = Schedule.data;
+                $ctrl.filter = formModel;
+                //show the list
+            }).catch(function() {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .clickOutsideToClose(true)
+                        .title('An error occurred')
+                        .ariaLabel('An error occurred')
+                        .ok('Okayyy')
+                        .openFrom('#filterSubmit')
+                );
+            }).then(function() {
+                $ctrl.gettingData = false;
+            });
+        }
+
+
     }
 })();
